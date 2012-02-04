9512bb289825ee7f8597fb202b713d022b7ae0eb
Markus Strauss <Markus@ITstrauss.eu>
2012-02-03 20:29:49 -0600

    now using file_line from puppet's stdlib to handle whole lines
    This example uses file_line: editfile::config { 'ensure a simple line
    is present': path => 'a file path', ensure => 'This line to be present'
    }
    
    file_line:
    https://github.com/puppetlabs/puppetlabs-stdlib/blob/master/lib/puppet/t
    ype/file_line.rb

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,18 +1,29 @@
-define editfile::config( $path, $entry, $ensure, $sep = '=', $quote = false ) {
+define editfile::config( $path, $entry = false, $ensure, $sep = '=', $quote = false ) {
 
-  if $quote == true {
-    $_ensure = "${entry}${sep}\"${ensure}\""
+  if $entry == false {
+    
+    file_line { $name:
+      path => $path,
+      line => $ensure,
+    }
+    
   } else {
-    $_ensure = "${entry}${sep}${ensure}"
-  }
-  
-  editfile { $title:
-    path    => $path,
-    match  => "^#?\s*${entry}\s?${sep}",
-    ensure => $ensure ? {
-      absent  => $ensure,
-      default => $_ensure,
-    },
+
+    if $quote == true {
+      $_ensure = "${entry}${sep}\"${ensure}\""
+    } else {
+      $_ensure = "${entry}${sep}${ensure}"
+    }
+
+    editfile { $title:
+      path   => $path,
+      match  => "^#?\s*${entry}\s?",
+      ensure => $ensure ? {
+        absent  => $ensure,
+        default => $_ensure,
+      },
+    }
+
   }
   
 }
