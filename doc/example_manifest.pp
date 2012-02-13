5f8cbb65511610ac35d25824bcdd3e22155fac51
Markus Strauss <Markus@ITstrauss.eu>
2012-02-12 18:18:27 -0600

    new postfix example

--- doc/example_manifest.pp
+++ doc/example_manifest.pp
@@ -26,3 +26,19 @@ editfile {
     ;
 }
 
+
+define postfix::maincf( $ensure ) {
+  editfile::config { "main.cf ${name}":
+    # require => Package[postfix],
+    path    => '/etc/postfix/main.cf',
+    entry   => $name,
+    sep     => ' = ',
+    ensure  => $ensure,
+    # notify  => Service[postfix],
+  }
+}
+
+postfix::maincf {
+  'relayhost':      ensure => 'mail.your.intranet.domain';
+  'notify_classes': ensure => '2bounce, resource, software';
+}
