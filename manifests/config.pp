03a9913e374b5f6410e8c9991bdd845cfe1aff59
Markus Strauss <Markus@ITstrauss.eu>
2012-02-12 18:12:18 -0600

    better comment and duplicate handling
    * now matching commented lines only if no other lines can be 'reused'
    * removing duplicate entries, just letting the last survive [this may
    or may not be exactly what you want]

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,21 @@
+# editfile::config
+#
+# This resource is an example of what you can do with editfile.
+# editfile::config manages simple configuration files which follow the scheme
+# PARAMETER = VALUE
+# A special feature is, that existing entries which are deactived with the pound sign, are reused.
+# E.g. >># PARAMETER = DEFAULT VALUE<< will we reused, but ONLY if there is no other line like >>PARAMETER = ANYTHING<<.
+#
+# @param[String]  $title  : any unique resource name
+# @param[String]  $path   : the file path
+# @param[String]  $entry  : which configuration entry to set
+# @param[String]  $ensure : set the configuration entry to this value, or use 'absent' to remove the entry $entry
+# @param[String]  $sep    : the separator to use, e.g. set to ' = '
+# @param[Boolean] $quote  : shall the value be quoted, like >> ENTRY = "value" <<
 define editfile::config( $path, $entry = false, $ensure, $sep = '=', $quote = false ) {
 
   if $entry == false {
-    
+
     file_line { $name:
       path => $path,
       line => $ensure,
@@ -14,14 +28,34 @@ define editfile::config( $path, $entry = false, $ensure, $sep = '=', $quote = fa
     } else {
       $_ensure = "${entry}${sep}${ensure}"
     }
-
-    editfile { $title:
+    
+    Editfile {
       path   => $path,
-      match  => "^#?\s*${entry}\s?",
-      ensure => $ensure ? {
-        absent  => $ensure,
-        default => $_ensure,
-      },
+    }
+
+    if $ensure == present {
+      editfile {
+        "$title":
+          # either match the with leading pound sign AND no other entry without pound - to replace the deactivated entry
+          # OR
+          # match simply without the pound sign - to replace an active entry
+          # OR
+          # if there is no match, editfile automatically appends our entry at EOF
+          match  => "/^(#?\s*${entry}\s?(?!.*^${entry})|${entry}\s?)/m",
+          ensure => present,
+          ;
+        "$title-cleanup":
+          # removing duplicate config entries;  the last entry shall survive
+          match  => "/^${_ensure}(?=.*^${_ensure})/m",
+          ensure => absent,
+          ;
+      }
+    } else {
+      editfile { "$title":
+        # we remove all matching entries, but not the comment lines
+        match  => "^${entry}${sep}",
+        ensure => absent,
+      }
     }
 
   }
